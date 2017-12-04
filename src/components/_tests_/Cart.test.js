95aece112c361a84214a5d4441e08550fe455c4b
diksha0562 <30476228+diksha0562@users.noreply.github.com>
2017-12-05 00:09:21 +0530

    Add files via upload

--- src/components/_tests_/Cart.test.js
+++ src/components/_tests_/Cart.test.js
@@ -1,22 +1,48 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import Cart from '../components/Cart';
-global.window = {};
-import localStorage from 'jest-localstorage-mock';
-window.localStorage = global.localStorage;
+require('jest-localstorage-mock');
 describe.only('<Cart>/',()=>{
-    const mockclear = jest.fn;
-    const wrapper = shallow(<Cart cart_data={[{product_type:'dress', price : '499'},{product_type:'top', price : '599'}]}/>);
-    // it('cart page',()=>{
-    //     //console.log(wrapper.debug());
-    // });
+    const baseProps = {
+        cart_data : [{product_type:'dress', price : '499'},{product_type:'top', price : '599'}],
+        onCartClear: jest.fn(),
+        onCartDelete : jest.fn()
+    }
+      const wrapper = shallow(<Cart {...baseProps}/>);
+    it('renders cart',()=>{
+        expect(wrapper.find(Cart)).toBeDefined();
+    });
     it('handle checkout function call',()=>{
-        expect(wrapper.find('.checkout-btn').length).toBe(1);
-        console.log(wrapper.instance().props);
-       // expect(sessionStorage.getItem('cart_counter')).toEqual(0);
-      expect(wrapper.instance().props.cart_data).toEqual([{product_type:'dress', price : '499'},{product_type:'top', price : '599'}]);
-        wrapper.find('.checkout-btn').simulate('click');
-       // expect(wrapper.find('.clear-btn').props().cart_data).toBe([]);
+       
+          expect(wrapper.find('.checkout-btn')).toBeDefined();
+         // wrapper.instance().handleCheckout();
+          expect(wrapper.instance().props.cart_data).toEqual([{product_type:'dress', price : '499'},{product_type:'top', price : '599'}]);
+        wrapper.find('.checkout-btn').simulate('click'); 
+
+    });
+    it('clear button functions check',()=>{
+        const KEY = 'cart_counter',
+        VALUE = '0';
+        expect(wrapper.find('.clear-btn')).toBeDefined();
+         wrapper.find('.clear-btn').simulate('click', {cart_data:[], onCartClear: jest.fn()});
+         expect(sessionStorage.setItem).toHaveBeenLastCalledWith(KEY, VALUE);
         
-    })
+         expect(sessionStorage.__STORE__[KEY]).toBe(VALUE);
+         expect(Object.keys(sessionStorage.__STORE__).length).toBe(1);
+        // expect(wrapper.instance().props.cart_data).toEqual([]);
+        //expect(baseProps.onCartClear).toHaveBeenCalledTimes(1);
+       // expect(baseProps.onCartClear).toHaveBeenCalledWith([]);
+    });
+    it('handle delete button',()=>{
+       // expect(wrapper.find(button)).toBeDefined();
+       const confirm = true ;
+      expect(wrapper.find('.delete-btn').length).toBe(2);
+      wrapper.find('.delete-btn').at(0).simulate('click');
+      wrapper.instance().handleDelete(0);
+
+      expect(wrapper.find('.delete-btn').length).toBe(2);
+     // expect(wrapper.instance().props.cart_data).toBe([{product_type:'top', price : '599'}]);
+        //wrapper.find('.delete-btn')[0].simulate('click',{});
+    });
+    
 })
